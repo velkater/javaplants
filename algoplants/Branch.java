4079f4614e47ef74de3655d476d886c4cb90efe8
velkater <tereza.velka@gmail.com>
2015-01-23 18:22:21 +0100

    vetev

--- algoplants/Branch.java
+++ algoplants/Branch.java
@@ -3,18 +3,21 @@ package algoplants;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.Cylinder;
+import javafx.scene.image.Image;
+
+import java.awt.*;
 
 public class Branch extends Cylinder{
 
     public Branch() {
-        this(50,200);
+        this(11,150);
     }
 
     public Branch(double radius, double height) {
         super(radius, height);
         PhongMaterial material = new PhongMaterial();
-        material.setDiffuseColor(Color.BURLYWOOD);
-        //material.setSpecularColor(Color.rgb(30, 30, 30));
+        material.setBumpMap(new Image("texture_wood2.jpg"));
+        material.setDiffuseColor(Color.web("D2BA6C"));
         this.setMaterial(material);
     }
 }
